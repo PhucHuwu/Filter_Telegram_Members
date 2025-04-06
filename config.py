5776b3c25b31c8220995f3ba61943b86c43fb0fb
PhucHuwu <phuctranhuu37@gmail.com>
2025-04-06 20:42:43 +0700

    add config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+bot_token = ''
+
+api_hash = ''
+api_id = ''
+
+chat_id = ''
+
+group_link = ''                            # link nhóm cần cào, ví dụ: 'https://t.me/abc'
+phone_number = ''                          # nhập số điện thoại có mã quốc gia, ví dụ: '+84123456789'
+messeges_limit = 1000                      # giới hạn lọc theo tin nhắn
+member_limit = 1000                        # giới hạn lọc member
+day_target = 2                             # lọc trong bao nhiêu ngày
